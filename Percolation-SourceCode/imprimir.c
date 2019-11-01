4515dc580cbead0e9c092b776358c01d48eceefb
Marcos <56776211+MTracchia@users.noreply.github.com>
2019-11-01 20:10:46 -0300

    Update imprimir.c

--- Percolation-SourceCode/imprimir.c
+++ Percolation-SourceCode/imprimir.c
@@ -8,13 +8,14 @@ int j;
 
 for(j=0;j<dim;j++)
 	{
-			for(k=0;k<dim;k++)
+		for(k=0;k<dim;k++)
 		
-				{
-					printf("%5d",*(red+dim*j+k));
-				}
+			{
+				printf("%5d",*(red+dim*j+k));
+			}
 	
-		printf("\n\n");
+	printf("\n\n");
+	
 	}
 
 return 0;
