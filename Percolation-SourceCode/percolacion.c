ce07c906334c45a3c0e11b753b0f63cdf34e8e9a
Marcos <56776211+MTracchia@users.noreply.github.com>
2019-11-01 20:30:40 -0300

    Update percolacion.c

--- Percolation-SourceCode/percolacion.c
+++ Percolation-SourceCode/percolacion.c
@@ -5,8 +5,6 @@ caso se indica la etiqueta del cluster percolante
 */
 
 void percolacion (int *red, int dim)
-
-
 {
 int i, j, k, l, m, x = 0, y = 0;// i y j son iteradores
 
@@ -16,58 +14,44 @@ int vec1[dim*dim];
 //Vector para la parte inferior de la matriz
 int vec2[dim*dim];
 
-
 //Inicializo los vectores de arriba 
 for (int l = 0;l < dim*dim; l++)
-  
   {   
     vec1[l]=0; 
     vec2[l]=0;
   }
 
-
 for (i = 0;i < dim; i++)  
-    
-    {
-      x = *(red + i);
-      *(vec1+x) = 1; 
-    }
+  {
+    x = *(red + i);
+    *(vec1+x) = 1; 
+  }
 
 *vec1 = 0;
 
-
 for (m = 0;m < dim; m++) 
-    
-    {
-      y = *(red + (m + dim*dim - dim));
-      *( vec2 + y ) = 1;    
-    }
+  {
+     y = *(red + (m + dim*dim - dim));
+     *( vec2 + y ) = 1;    
+  }
 
 *vec2 = 0;
-
 j=2;
-
-
+  
 while (j < dim*dim && vec1[j]*vec2[j]==0)
+  {
+     j++;
+  }
     
-    {
-      j++;
-    }
-    
-
 if (j == dim*dim) 
-    
-    {
-      j=0;
-      k=j;
-    }
-
-      else 
-        {
-          k=j;
-        }
-
-
+  {
+     j=0;
+     k=j;
+  }
+     else 
+       {
+         k=j;
+       }
+  
 k == 0 ? printf("No percolo\n\n"):printf("Cluster percolante %d\n\n",k);
-
 }
