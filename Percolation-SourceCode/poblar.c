c585f42729776aef95c206c37a85bc90ab68683f
Marcos <56776211+MTracchia@users.noreply.github.com>
2019-11-04 20:44:55 -0300

    Update poblar.c

--- Percolation-SourceCode/poblar.c
+++ Percolation-SourceCode/poblar.c
@@ -1,21 +1,15 @@
 //#include <stdio.h>
 
 int poblar(int dim, float p, int *red)
-
 {
-
 int i;
 float random;
-
 for(i=0;i<dim*dim;i++)
 	{
 		random = ((float)rand()/(float)RAND_MAX);
 		*(red+i)=0;
-
-		if (random<p)
-     	*(red+i)=1;
+			if (random<p)
+     			*(red+i)=1;
 	}
-
 return 0;
-
 }
