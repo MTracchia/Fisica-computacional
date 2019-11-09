68d0b96dac89ab37fb83933fba130afd5561c11e
Marcos <56776211+MTracchia@users.noreply.github.com>
2019-11-09 17:10:24 -0300

    Update set_box.c

--- Molecular-Dynamics/set_box.c
+++ Molecular-Dynamics/set_box.c
@@ -6,15 +6,16 @@ int n=(int)cbrt(N), i=0; //n=(int)cbrt(N) para que no sobren espacios
 float dl=(float)L/cbrt(N);    
 for (int x=0;x<n;x++)
 {	
-	for (int y=0;y<n;y++)		
-	   {	
-		for (int z=0;z<n;z++)
-		    {  
-			*(posicion + 3*i)=(float)(dl*((float)x+0.5));
-			*(posicion + 3*i+1)=(float)dl*((float)y+0.5);
-			*(posicion + 3*i+2)=(float)dl*((float)z+0.5);
-		     	i++;
-		    }
-	    }  
+    for (int y=0;y<n;y++)		
+    {	
+	for (int z=0;z<n;z++)
+	{  
+	*(posicion + 3*i)=(float)(dl*((float)x+0.5));
+	*(posicion + 3*i+1)=(float)dl*((float)y+0.5);
+	*(posicion + 3*i+2)=(float)dl*((float)z+0.5);
+	i++;
+	}
+     }  
+
 }
 }
