14755f8bb233b92e0ca1d6d81ce403bbaa960432
Marcos <56776211+MTracchia@users.noreply.github.com>
2019-11-17 23:40:53 -0300

    Add files via upload

--- Axelrod-Model/agentes.c
+++ Axelrod-Model/agentes.c
@@ -5,6 +5,8 @@
 
 #define L 10
 #define F 4
+#define Q 20	
+
 
 
 struct agente
@@ -16,15 +18,25 @@ int atributos[F];
 int main()
 {
 
+srand(time(NULL));
 struct agente red[L*L];
+int q = 20, q_value = 0;
 
 //red =(int*)malloc(L*L*sizeof(struct agente));
 //*(red+3)->atributos[4]=3;	
 
-//Arrancar con inicializacion
 
-red[12].atributos[3]=6;
-printf("%d\n",red[12].atributos[3]);
+//Inicializa los estados de los agentes y también los imprime
+for (int i = 0; i < L*L; i++)
+{
+	for (int j = 0; j < F; j++)
+	{
+		q_value = rand() % Q;
+		red[i].atributos[j]=q_value;
+		printf("%5d",red[i].atributos[j]);
+	}
+printf("\n");
+}
 
 return 0;
 }
