00f2eb353e1b0438abc2a9e7f6eb89c9dfddea79
Marcos <56776211+MTracchia@users.noreply.github.com>
2019-11-25 20:50:10 -0300

    Add files via upload

--- Axelrod-Model/main1.c
+++ Axelrod-Model/main1.c
@@ -5,7 +5,7 @@
 
 #define RAND_MAX 2147483647
 
-#define L 10
+#define L 4
 #define F 4
 #define Q 2
 
@@ -24,8 +24,13 @@ int main()
 srand(time(NULL));
 struct agente red[L*L];
 
+
+
 inicializacion(red);
 Interaccion(red);
+
+
+
 return 0;
 }
 
@@ -43,22 +48,108 @@ for (int i = 0; i < L*L; i++)
 printf("\n");
 }
 //------------------------------------------------------------ELEMENTO_ACTIVO
-//Selecciono un agente al azar generando un número random emtre 0 a L*L.
+//Selecciono un agente al azar generando un número random entre 0 a L*L.
 int elemento_activo(void) {return rand() % (L*L);}
-//-------------------------------------------------------------ELEMENTO_VECINO
+
+
+
+
+
+
+
+//------------------------------------------------------------ELEMENTO_VECINO
 int elemento_vecino (void) {
+
+
 int elemento_activo_random, i, j;
 //Convierto el vector de agentes en una matriz de agentes
 elemento_activo_random = elemento_activo();
-i=(float)elemento_activo_random/(float)L; //columna
-j=elemento_activo_random%L; //fila
+i=0; //columna   // n = i*L+j
+j=3; //fila
+
+int i_izq, i_der, j_sup, j_inf;
+
+
+int random_vecino;
+int indice_vecino;
+i_izq = i - 1;
+i_der = i + 1;
+j_sup = j - 1;
+j_inf = j + 1;
+
+int vecAux[] = {i_izq,i_der,j_inf,j_sup};
+int auxiliar;
+
+
+do {
+	random_vecino = rand() % 4;
+	
+} while ( (vecAux[random_vecino] <0) || (vecAux[random_vecino] >=L));
+
+auxiliar = vecAux[random_vecino];
+
+
+
+if (random_vecino == 0)
+{
+	indice_vecino = auxiliar*L + j;
+} 
+else if (random_vecino == 1)
+{
+	indice_vecino = auxiliar*L + j;
 }
+else if (random_vecino == 2)
+{
+	indice_vecino = i*L + auxiliar;
+}
+else if (random_vecino == 3)
+{
+	indice_vecino = i*L + auxiliar;
+}
+
+
+
+
+
+
+
+/*
+do	{	
+		random_vecino = rand() % 4;
+			if (random_vecino == 0)
+				{
+					i--; //-= +=
+				}
+			else if (random_vecino == 1)
+				{
+					j--;
+				}
+			else if (random_vecino == 2)
+				{
+					i++;
+				}
+			else if (random_vecino == 3)
+				{
+					j++;
+				}
+	} while  (i < 0 || i >= L || j < 0 || j >= L);
+int indice_vecino;
+indice_vecino = (i*L) + j;
+i=0;
+j=0;
+*/
+
+return indice_vecino;
+}
+
 //-------------------------------------------------------------INTERACCION
 void Interaccion (struct agente red[]){
 int similitudes = 0, diferencias = 0, vector_diferencias[F], componente_random, atributo_seleccionado;
 float homofilia = 0.0, random;
 /*Comparo el agente activo con su vecino, componente a componente (o atributo por atributo).
 Por cada atributo que ambos tengan en común, la variable similitudes sube una unidad.*/
+
+/*
 for (int i = 0; i < F; i++)
 	{  
 		red[0].atributos[i];  
@@ -77,7 +168,7 @@ printf("random es %f\n",random);
 /*La interaccion funciona de acuerdo a dos criterios; si random<homofilia Y si la cantidad 
 de similitudes es distinta de F. Notar que si los dos agentes son iguales (similitud=F) no 
 es necesaria la interacción, de ahí al operador &&.*/
-if((random < homofilia)  && (similitudes != F))	
+/*if((random < homofilia)  && (similitudes != F))	
 	{					
 		for (int j = 0; j < F ; j++)
 			{
@@ -88,9 +179,35 @@ if((random < homofilia)  && (similitudes != F))
    					}
 			}	
 		componente_random = rand() % diferencias; 
-		atributo_seleccionado = vector_diferencias[componente_random];
-		red[0].atributos[atributo_seleccionado]=red[1].atributos[atributo_seleccionado];
+		atributo_seleccionado = vector_diferencias[componente_random]; //Selecciono el índice del atributo que va a cambiar debido a la interacción
+		red[0].atributos[atributo_seleccionado]=red[1].atributos[atributo_seleccionado]; //Actualización del atributo del agente activo
 	}  
+*/
+
+
+//------------------------------------------------------------------------------------------
+
+//int indice = elemento_activo();
+int indiceVec = elemento_vecino();
+
+
+
+printf("\n");
+
+for (int i = 0; i < F; i++){
+				printf("%4d",red[0].atributos[i]);
+}
+
+printf("\n");
+
+
+for (int j = 0; j < F; j++){
+				printf("%4d",red[indiceVec].atributos[j]);
+}
+
+printf("\n");
+printf("%d\n", indiceVec);
+
 }
 
 
