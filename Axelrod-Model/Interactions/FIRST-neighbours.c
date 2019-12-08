9d71fd0714e55b542745802811bdc5e370553ed9
Marcos <56776211+MTracchia@users.noreply.github.com>
2019-12-08 20:36:00 -0300

    Update FIRST-neighbours.c

--- Axelrod-Model/Interactions/FIRST-neighbours.c
+++ Axelrod-Model/Interactions/FIRST-neighbours.c
@@ -8,7 +8,7 @@ int i_left = i - 1;
 int i_right = i + 1;
 int j_up = j - 1;
 int j_down = j + 1;
-/*First neighbours*/
+/*First neighbour*/
 int vecNeigh[] = {i_left,i_right,j_up,j_down};
 /*SELECTION PROCESS*/
 do {
