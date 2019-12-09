a3eecbd10214671ae944cb2066b7ce0b0cd6ae72
Marcos <56776211+MTracchia@users.noreply.github.com>
2019-12-08 23:17:39 -0300

    Add files via upload

--- Axelrod-Model/SRC/main.c
+++ Axelrod-Model/SRC/main.c
@@ -14,7 +14,6 @@ typedef struct agent
 	int features[F];
 } AGENT; 
 
-
 void INITIALIZATION(AGENT grid[]);
 int SIMILARITIES (AGENT grid[], int active_agent, int first_neighbour);
 int ACTIVE_AGENT (void);
@@ -27,8 +26,6 @@ int main()
 	srand(time(NULL));
 	AGENT grid[L*L];
 	int active_agent = 0, first_neighbour_agent = 0;
-
-
 
 	//grid =(int*)malloc(L*L*sizeof(struct agente));
 	INITIALIZATION(grid);
@@ -61,6 +58,7 @@ void INITIALIZATION (AGENT grid[])
 			{
 				for (int j = 0; j < F; j++)
 					{
+						/*Each attribute is assigned a value between 0 and Q*/
 						grid[i].features[j] = rand() % Q;
 						printf("%4d",grid[i].features[j]);
 					}
@@ -71,6 +69,7 @@ void INITIALIZATION (AGENT grid[])
 //*************************************************SIMILARITIES*************************************************
 int SIMILARITIES (AGENT grid[], int active_agent, int first_neighbour)  
 	{
+		/*This function returns the number of equals attributes of the two agents selected*/
 		int similarity = 0;
 		for (int i = 0; i < F; i++)
 			{  
@@ -81,13 +80,12 @@ int SIMILARITIES (AGENT grid[], int active_agent, int first_neighbour)
 			}
 		return similarity;	
 	}
-
 //*************************************************ACTIVE-AGENT*************************************************
 int ACTIVE_AGENT (void)
 	{
+		/*Generation of a random number from 0 to L*L for select an agent.*/
 		return (rand() % (L*L));
 	}
-
 //*************************************************HOMOPHILIA*************************************************
 float HOMOPHILIA (int similarity)
 	{
@@ -96,90 +94,48 @@ float HOMOPHILIA (int similarity)
 //*************************************************FIRST-NEIGHBOURS*************************************************
 int FIRST_NEIGHBOURS(int active_agent)
 	{	
-		/*I generate a random number from 0 to L*L called "random_active_element" (from a vector).
-		Then, turn it to matrix notation of the form  n = i*L+j in "i" and "j" through the "/" and "%" division*/
+		/*I turn the "active_agent" (from a vector) to matrix notation of the form  n = i*L+j 
+		in "i" and "j" through the "/" and "%" division*/
 		int i=active_agent/L;
 		int j=active_agent%L;
-		int first_neighbour, random_neighbour,random_fn;
-		/*Calculate the first_neighbours*/
+		/*Calculate the first_neighbour_agent*/
 		int i_left = i - 1;
 		int i_right = i + 1;
 		int j_up = j - 1;
 		int j_down = j + 1;
-		/*First_neighbours*/
+		/*First neighbours vector*/
 		int vecNeigh[] = {i_left,i_right,j_up,j_down};
+		int first_neighbour = 0,random_fn = 0;
 		/*SELECTION PROCESS*/
 		do {
 				random_fn = rand() % 4; 
 			} while ( (vecNeigh[random_fn] <0) || (vecNeigh[random_fn] >=L));
-		random_neighbour = vecNeigh[random_fn];
 		if (random_fn == 0)
-			{first_neighbour = random_neighbour*L + j;} 
+			{first_neighbour = vecNeigh[random_fn]*L + j;} 
 				else if (random_fn == 1)
-					{first_neighbour = random_neighbour*L + j;}
+					{first_neighbour = vecNeigh[random_fn]*L + j;}
 						else if (random_fn == 2)
-							{first_neighbour = i*L + random_neighbour;}
-								else if (random_fn == 3){first_neighbour = i*L + random_neighbour;}
+							{first_neighbour = i*L + vecNeigh[random_fn];}
+								else if (random_fn == 3){first_neighbour = i*L + vecNeigh[random_fn];}
 		return first_neighbour;
 	}
-
 //*************************************************INTERACTION*************************************************
 void INTERACTION (AGENT grid[], int active_agent, int first_neighbour_agent)
 	{
 		int similarity = 0,random_feature = 0;
 		float homophilia = 0.0, random_for_prob_interaction = 0.0;
-		
+		/*Similirity is the amount of attributes of the agents.*/
 		similarity = SIMILARITIES(grid,active_agent,first_neighbour_agent);
+		/*Homophilia is the degree of kinship of the two agents.*/
 		homophilia = HOMOPHILIA(similarity);
-		
+		/*This random number serves for compare it with the homophilia.*/
 		random_for_prob_interaction = (float)rand()/(float)RAND_MAX;
-		
-
+		/*INTERACTION PROCESS. Note that if the agents are exactly equals there's no interaction.*/
 		if((random_for_prob_interaction < homophilia)  && (similarity != F))			
 			{	
 				do {random_feature = rand() % F;} 
 					while (grid[active_agent].features[random_feature] == grid[first_neighbour_agent].features[random_feature]);
 				grid[active_agent].features[random_feature] = grid[first_neighbour_agent].features[random_feature];
 				printf(" similarity:%d,   homophilia:%f\n", similarity, homophilia);
-				
-
-				//Forma antigua de cambiar los atributos en caso de interaccion
-				/*
-				for (int j = 0; j < F ; j++)
-					{
-		   				if (grid[0].features[j] != grid[1].features[j])
-		   					{   							
-			   					vector_diferencias[diferencias] = j;
-			   					diferencias++;
-		   					}
-					}	
-				componente_random = rand() % diferencias; 
-				atributo_seleccionado = vector_diferencias[componente_random];
-				grid[0].features[atributo_seleccionado]=grid[1].features[atributo_seleccionado];
-				*/ 
-	
 			} 
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
